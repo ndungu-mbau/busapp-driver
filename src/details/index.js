0ba6b8b476a16fe42a979b9bc752e58ef3a24cae
Nelson Mbau <mbau.ndungu@gmail.com>
2020-09-23 19:29:18 +0300

    fix: Fixes by eslint/prettier

--- src/details/index.js
+++ src/details/index.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-import { View, Text } from 'react-native'
+import {View, Text, StyleSheet} from 'react-native';
 
-const Details = ({ route }) => {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen: {route.params.user_data.id}</Text>
-      </View>
-    )
-}
+const Details = ({route}) => {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen: {route.params.user_data.id}</Text>
+    </View>
+  );
+};
 
-export default Details
+export default Details;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
